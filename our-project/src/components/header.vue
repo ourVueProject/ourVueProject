cfd892c798be3929de874266e872468180b8d6a6
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-12-25 14:17:48 +0800

    update
    增加重置样式

--- our-project/src/components/header.vue
+++ our-project/src/components/header.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>toubu</div>
+    <div class="head displayflex">
+        <img src="../assets/logo.png">
+    </div>
 </template>
 
 <script>
@@ -8,5 +10,13 @@ export default {}
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.head {
+    width:100%;
+}
+.displayflex {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+}
 </style>
