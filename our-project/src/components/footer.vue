a2a4785d842ba4a7f17f47a81b232a0f5951c794
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-12-25 15:04:39 +0800

    update

--- our-project/src/components/footer.vue
+++ our-project/src/components/footer.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="foot">
+        <div class="displayflex">
+            <div>版权信息</div>
+            <div>联系我们</div>
 
+        </div>
     </div>
 </template>
 
@@ -10,8 +14,16 @@ export default {}
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.displayflex {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+}
 .foot {
     width:100%;
     background:#393939;
+    padding:40px 0;
+    color:#fff;
 }
 </style>
