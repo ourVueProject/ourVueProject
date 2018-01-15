2e35847f04f9ee8f1219bcc57042c3754f2c960c
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2018-01-15 16:10:21 +0800

    测试WebSocket

--- our-project/src/components/header.vue
+++ our-project/src/components/header.vue
@@ -40,7 +40,10 @@ export default {
     },
     methods: {
         login() {
-
+            if ("WebSocket" in window)
+            {
+               alert("您的浏览器支持 WebSocket!");
+           }
         }
     }
 }
